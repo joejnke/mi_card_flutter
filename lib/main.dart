4fb363ce580e2ccc286720785a87f78542c85edc
Kirubel Kassaye <KirubelKa@moenco.corp>
2022-05-18 15:07:50 +0300

    done layout excersise

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,51 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                  color: Colors.red,
+                  width: 100.0
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ColoredBox(
+                    color: Colors.yellow,
+                    child: SizedBox(
+                        width: 100.0,
+                        height: 100.0
+                    ),
+                  ),
+                  ColoredBox(
+                    color: Colors.green,
+                    child: SizedBox(
+                        width: 100.0,
+                        height: 100.0
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                  color: Colors.blue,
+                  width: 100.0
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
